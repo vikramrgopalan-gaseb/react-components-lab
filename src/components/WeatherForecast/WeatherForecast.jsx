6605f91bbbbf1de28c5b1654ad624bb363ea0970
vikramgopalan-gaseb <vikram.r.gopalan@gmail.com>
2026-02-03 18:54:23 -0700

    2/3 setup v3

--- src/components/WeatherForecast/WeatherForecast.jsx
+++ src/components/WeatherForecast/WeatherForecast.jsx
@@ -1,5 +1,17 @@
 import './WeatherForecast.css'
 
+function WeatherForecast (props) {
+    const { day } = props
+    return (
+        <>
+            <li>
+                <h3>It is {day.day}</h3>
+                <p>The forecast is: {day.conditions}</p>
+                <p>The time is: {day.time}</p>
+            </li>
+        </>
+    )
+}
 
 export default WeatherForecast;
 
